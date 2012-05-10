d9ba57106398b943022c077422f9f2a1cc07c33a
Derek Mueller <dtmuelle@ucsc.edu>
2012-05-09 22:16:56 -0700

    fancy formatting for test1.py

--- file_manager/test1.py
+++ file_manager/test1.py
@@ -3,26 +3,59 @@ import file_manager
 import patient
 import datetime
 
-p1 = patient.patient ('p1', 'tmp.img', 'tmp desc', 'tmp')
+p1 = patient.patient ('AbdoRehima', 'tmp.img', 'tmp desc', 'tmp')
 p1.date = datetime.datetime.now ()
-p1.UUID = '001'
+p1.UUID = '12345678910'
 
 fm = file_manager.FileManager ()
 
+print "\n"
+print "////////////////////// Starting Test 1 /////////////////////"
+print "\n"
+print "calling fm.create_clinic_dir ('my_clinic', '.')"
+print "\n"
+
+
 fm.create_clinic_dir ('my_clinic', '.')
 
-fm.get_clinic_dir ()
+print "calling fm.get_clinic_dir ()"
+print "\n"
+
+print "clinic_dir = ", fm.get_clinic_dir ()
+print "\n"
+
+print "saving patient Abdo Rehima's files" 
+print ("calling fm.save_patient_files ",
+       "(p1, 'tmp.pkl', 'tmp.docx', 'tmp2.img')")
+print "\n"
 
 fm.save_patient_files (p1, 'tmp.pkl', 'tmp.docx', 'tmp2.img')
 
-fm.save_patient_files (p1, '001', 'tmp.pkl', profile_image='tmp.img')
+print "saving patient Abdo Rehima's files" 
+print ("calling  fm.save_patient_files (p1, 'tmp.pkl', ",
+       "profile_image='tmp.img')")
+print "\n"
 
-fm.save_patient_files (p1, '001', 'tmp.xml')
+fm.save_patient_files (p1, 'tmp.pkl', 
+                       profile_image='tmp.img')
 
-files = fm.get_patient_files ('001')
+print "saving patient Abdo Rehima's files" 
+print "calling fm.save_patient_files (p1, 'tmp.xml')"
+print "\n"
+
+fm.save_patient_files (p1, 'tmp.xml')
+
+print "retrieving patient Abdo Rehima's files" 
+print "calling fm.get_patient_files ('12345678910')"
+print "\n"
+
+files = fm.get_patient_files ('12345678910')
 
 print 'files = ' + ' '.join (files)
 
 
+print "\n"
+print "////////////////////// Test 1 Complete /////////////////////"
+print "\n"
 
 
